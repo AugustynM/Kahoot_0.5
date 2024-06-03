717274e32e9857b70d5eeecf660b006f4e3139d6
AugustynM <augustyn.majtyka@student.uj.edu.pl>
2024-06-03 20:21:38 +0200

    gameview listens for broadcasts

--- src/main/java/pl/tcs/po/views/game/GameView.java
+++ src/main/java/pl/tcs/po/views/game/GameView.java
@@ -2,6 +2,9 @@ package pl.tcs.po.views.game;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.DetachEvent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -11,11 +14,13 @@ import com.vaadin.flow.router.OptionalParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.shared.Registration;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Horizontal;
 
 import jakarta.annotation.PostConstruct;
 import pl.tcs.po.model.GameModel;
 import pl.tcs.po.model.QuestionModel;
+import pl.tcs.po.service.GameBroadcaster;
 import pl.tcs.po.service.GameService;
 import pl.tcs.po.views.MainLayout;
 import pl.tcs.po.webClient.QuestionsClient;
@@ -29,6 +34,8 @@ public class GameView extends VerticalLayout implements HasUrlParameter<Integer>
 
     VerticalLayout questionLayout;
     VerticalLayout errorLayout = new GameNotFoundLayout();
+
+    Registration gameBroadcasterRegistration;
 
     private QuestionModel question;
     private GameModel gameModel = null;
@@ -59,14 +66,43 @@ public class GameView extends VerticalLayout implements HasUrlParameter<Integer>
 
             if (gameModel != null && gameModel.getCurrentQuestionModel() != null) {
                 question = gameModel.getCurrentQuestionModel();
-                HorizontalLayout layout = new HorizontalLayout(new QuestionLayout(question), new PlayerListLayout(gameModel));
+                HorizontalLayout layout = new HorizontalLayout(new QuestionLayout(question),
+                        new PlayerListLayout(gameModel));
                 layout.setWidthFull();
                 this.replace(questionLayout, layout);
-            }
-            else {
+            } else {
                 replace(questionLayout, errorLayout);
             }
         }
+    }
+
+    private void refresh() {
+        if (gameModel != null && gameModel.getCurrentQuestionModel() != null) {
+            question = gameModel.getCurrentQuestionModel();
+            HorizontalLayout layout = new HorizontalLayout(new QuestionLayout(question),
+                    new PlayerListLayout(gameModel));
+            layout.setWidthFull();
+            this.replace(questionLayout, layout);
+        } else {
+            replace(questionLayout, errorLayout);
+        }
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        UI ui = attachEvent.getUI();
+        gameModel = gameService.getGame(gameId);
+        gameBroadcasterRegistration = GameBroadcaster.register(game -> {
+            ui.access(() -> {
+                refresh();
+            });
+        }, gameModel);
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        gameBroadcasterRegistration.remove();
+        gameBroadcasterRegistration = null;
     }
 
 }
